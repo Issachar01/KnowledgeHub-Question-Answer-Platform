9f6891437ae0cb79e8264127e3464613b7f7dadb
Yisacor Dereje <issachardereje010@gmail.com>
2026-09-07 13:27:43 +0300

    Delete src/routes/questionQueryRoutes.js

--- src/routes/questionQueryRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/questionQueryRoutes.js (Branch: feature/search-pagination-sorting)
-const express = require('express');
-const router = express.Router();
-const { getFilteredQuestions } = require('../controllers/questionQueryController');
-
-router.get('/questions', getFilteredQuestions);
-
-module.exports = router;
